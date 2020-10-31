286d96a703d67b5259cedfa9244660bd0184357a
Omer Oreg <sarhatabaot1@gmail.com>
2020-10-31 15:57:23 +0200

    Added support for playlists

--- src/main/java/se/walkercrou/composer/Composer.java
+++ src/main/java/se/walkercrou/composer/Composer.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.entity.living.player.Player;
@@ -31,130 +32,175 @@ import java.util.*;
  * Main class for Composer plugin.
  */
 @Plugin(id = "composer",
-        name = "Composer",
-        url = "https://github.com/sarhatabaot/Composer",
-        authors = { "windy","sarhatabaot" })
+		name = "Composer",
+		url = "https://github.com/sarhatabaot/Composer",
+		authors = {"windy", "sarhatabaot"})
 public class Composer {
-    @Getter @Setter
-    private static Composer instance;
+	@Getter
+	@Setter
+	private static Composer instance;
 
-    @Inject
-    @Getter
-    private Logger logger;
-    @Inject @DefaultConfig(sharedRoot = false)
-    private Path configPath;
-    @Inject @DefaultConfig(sharedRoot = false)
-    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+	@Inject
+	@Getter
+	private Logger logger;
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	private Path configPath;
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	private ConfigurationLoader<CommentedConfigurationNode> configLoader;
 
-    @Getter
-    private ConfigurationNode config;
-    private final List<NoteBlockStudioSong> nbsTracks = new ArrayList<>();
-    private final Map<UUID, MusicPlayer> musicPlayers = new HashMap<>();
+	@Getter
+	private ConfigurationNode config;
 
-    @Listener
-    public void onGameStarted(GameStartedServerEvent event) {
-        setupConfig();
-        setInstance(this);
-        if (config.getNode("debugMode").getBoolean())
-            new TestCommands(this).register();
-        new ComposerCommands(this).register();
-        Task.Builder taskBuilder = Task.builder();
-        taskBuilder.execute(new LoadTracksRunnable()).submit(this);
-        //loadTracks();
-    }
+	@Getter
+	private final Map<String, Playlist> playlists = new HashMap<>();
 
-    /**
-     * Returns the specified player's {@link MusicPlayer}.
-     * If one does not exist a new one will be created.
-     *
-     * @param player player
-     * @return music player
-     */
-    public MusicPlayer getMusicPlayer(Player player) {
-        UUID playerId = player.getUniqueId();
-        MusicPlayer mp = musicPlayers.get(playerId);
-        if (mp == null) {
-            mp = new MusicPlayer(this, getNbsTracks());
-            musicPlayers.put(playerId, mp);
-        }
-        return mp;
-    }
+	private final List<NoteBlockStudioSong> nbsTracks = new ArrayList<>();
+	private final Map<UUID, MusicPlayer> musicPlayers = new HashMap<>();
 
-    /**
-     * Returns the currently loaded {@link NoteBlockStudioSong}s.
-     *
-     * @return list of tracks
-     */
-    public List<NoteBlockStudioSong> getNbsTracks() {
-        return Collections.unmodifiableList(nbsTracks);
-    }
+	@Listener
+	public void onGameStarted(GameStartedServerEvent event) {
+		setupConfig();
+		setInstance(this);
+		if (config.getNode("debugMode").getBoolean())
+			new TestCommands(this).register();
+		new ComposerCommands(this).register();
+		Task.Builder taskBuilder = Task.builder();
+		taskBuilder.execute(new LoadTracksRunnable()).submit(this);
+		//loadTracks();
+	}
 
-    public class LoadTracksRunnable implements Runnable {
+	/**
+	 * Returns the specified player's {@link MusicPlayer}.
+	 * If one does not exist a new one will be created.
+	 *
+	 * @param player player
+	 * @return music player
+	 */
+	public MusicPlayer getMusicPlayer(Player player) {
+		UUID playerId = player.getUniqueId();
+		MusicPlayer mp = musicPlayers.get(playerId);
+		if (mp == null) {
+			if(config.getNode("use-playlists").getBoolean())
+				mp = new MusicPlayer(this, playlists.get(config.getNode("default-playlist").getString()).getTracks());
+			else
+				mp = new MusicPlayer(this, getNbsTracks());
+			musicPlayers.put(playerId, mp);
+		}
+		return mp;
+	}
 
+	/**
+	 * Returns the currently loaded {@link NoteBlockStudioSong}s.
+	 *
+	 * @return list of tracks
+	 */
+	public List<NoteBlockStudioSong> getNbsTracks() {
+		return Collections.unmodifiableList(nbsTracks);
+	}
 
-        @Override
-        public void run() {
-            File file = new File(configPath.toFile().getParentFile(), "tracks");
-            if (!file.exists()) {
-                file.mkdirs();
-                logger.info("Created tracks folder.");
-            }
-
-            double progress = 0;
-            int total = Objects.requireNonNull(file.list((d, n) -> n.endsWith(".nbs"))).length;
-            progress(progress);
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(file.toPath(), "*.nbs")) {
-                for (Path path : stream) {
-                    if(addTrack(path))
-                        progress(++progress / total * 100);
-                }
-                logger.info(String.format("Loaded %d/%d tracks.",nbsTracks.size(),total));
-            } catch (IOException e) {
-                logger.error("An error occurred while loading the tracks.", e);
-            }
-        }
+	public class LoadTracksRunnable implements Runnable {
 
 
-        private void progress(double p) {
-            getLogger().info("Loading tracks: " + (int) p + "%");
-        }
+		@Override
+		public void run() {
+			File file = new File(configPath.toFile().getParentFile(), "tracks");
+			if (!file.exists()) {
+				file.mkdirs();
+				logger.info("Created tracks folder.");
+			}
+
+			if (config.getNode("use-playlists").getBoolean()) {
+				File playlists = new File(configPath.toFile().getParent(), "playlists");
+				if (!playlists.exists()) {
+					playlists.mkdirs();
+					logger.info("Created playlist folder.");
+				}
+				file = new File(configPath.toFile().getParentFile(), "playlists/" + config.getNode("default-playlist").getString());
+				if (!file.exists()) {
+					file.mkdirs();
+					logger.info("Created default playlist folder.");
+				}
+				for(File playlistDir: playlists.listFiles()){
+					if(playlistDir.isDirectory())
+						loadTracks(playlistDir);
+				}
+			} else {
+				loadTracks(file);
+			}
+
+		}
 
 
-        private boolean addTrack(final Path path){
-            try {
-                nbsTracks.add(NoteBlockStudioSong.read(path.toFile()));
-                return true;
-            } catch (IOException| CorruptedFileException e) {
-                logger.error("Could not read file (file is likely malformed): %s",path);
-                logger.debug(e.getMessage());
-            }
-            return false;
-        }
-    }
+		private void loadTracks(final File file) {
+			double progress = 0;
+			int total = Objects.requireNonNull(file.list((d, n) -> n.endsWith(".nbs"))).length;
+			progress(progress);
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(file.toPath(), "*.nbs")) {
+				List<NoteBlockStudioSong> songs = new ArrayList<>();
+				for (Path path : stream) {
+					NoteBlockStudioSong song = addTrack(path);
+					if (song != null) {
+						progress(++progress / total * 100);
+						songs.add(song);
+					}
+				}
+				addTracks(file.getName(),songs);
+				logger.info(String.format("Loaded %d/%d tracks.", songs.size(), total));
+			} catch (IOException e) {
+				logger.error("An error occurred while loading the tracks.", e);
+			}
+		}
 
 
+		private void progress(double p) {
+			getLogger().info("Loading tracks: " + (int) p + "%");
+		}
 
-    private void setupConfig() {
-        File file = configPath.toFile();
-        if (!file.exists())
-            createDefaultConfig(file);
-        try {
-            config = configLoader.load();
-        } catch (IOException e) {
-            getLogger().info(e.getMessage());
-            getLogger().error("An error occurred while loading the config file.", e);
-        }
-    }
+		public void addTracks(final String name, final List<NoteBlockStudioSong> songs){
+			if(config.getNode("use-playlists").getBoolean()){
+				playlists.put(name,new Playlist(songs));
+			} else {
+				nbsTracks.addAll(songs);
+			}
+		}
 
-    private void createDefaultConfig(File file) {
-        try {
-            file.getParentFile().mkdirs();
-            file.createNewFile();
-            Files.copy(Composer.class.getResourceAsStream("/assets/se/walkercrou/composer/default.conf"), configPath,
-                    StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            getLogger().error(e.getMessage());
-            logger.error("An error occurred while creating default config.", e);
-        }
-    }
+
+		@Nullable
+		private NoteBlockStudioSong addTrack(final Path path) {
+			try {
+				return NoteBlockStudioSong.read(path.toFile());
+			} catch (IOException | CorruptedFileException e) {
+				logger.error("Could not read file (file is likely malformed): %s", path);
+				logger.debug(e.getMessage());
+			}
+			return null;
+		}
+	}
+
+
+	private void setupConfig() {
+		File file = configPath.toFile();
+		if (!file.exists())
+			createDefaultConfig(file);
+		try {
+			config = configLoader.load();
+		} catch (IOException e) {
+			getLogger().info(e.getMessage());
+			getLogger().error("An error occurred while loading the config file.", e);
+		}
+	}
+
+	private void createDefaultConfig(File file) {
+		try {
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+			Files.copy(Composer.class.getResourceAsStream("/assets/se/walkercrou/composer/default.conf"), configPath,
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			getLogger().error(e.getMessage());
+			logger.error("An error occurred while creating default config.", e);
+		}
+	}
 }
